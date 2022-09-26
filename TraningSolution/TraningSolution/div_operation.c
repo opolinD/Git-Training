83c00e83a70c5d962c24549c6f0fb61291506218
Dmitry Alekseevich <opolindmitry@gmail.com>
2022-09-26 20:54:08 +0300

    исправлен баг с отрицательным нулем

--- TraningSolution/TraningSolution/div_operation.c
+++ TraningSolution/TraningSolution/div_operation.c
@@ -1,4 +1,5 @@
 #include <stdio.h> 
+#include<math.h>
 int main() {
 	int a, b;
 	float div;
@@ -6,6 +7,10 @@ int main() {
 	scanf_s("%d", &b);
 	if (b == 0) {
 		printf("b can't bo 0");
+	}else if (a == 0) {
+		div = abs(a / b);
+		printf("%.2f", div);
+
 	}else {
 		div = a / b;
 		printf("%.2f", div);
