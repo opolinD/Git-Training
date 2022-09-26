b6c053b08bb0c53003fc00b1ccf680183d7b6375
Dmitry Alekseevich <opolindmitry@gmail.com>
2022-09-26 20:59:59 +0300

    добавлены printf

--- TraningSolution/TraningSolution/div_operation.c
+++ TraningSolution/TraningSolution/div_operation.c
@@ -3,17 +3,18 @@
 int main() {
 	int a, b;
 	float div;
+	pritnf("Input a and b: ")
 	scanf_s("%d", &a);
 	scanf_s("%d", &b);
 	if (b == 0) {
 		printf("b can't bo 0");
 	}else if (a == 0) {
 		div = abs(a / b);
-		printf("%.2f", div);
+		printf("Result of division: %.2f", div);
 
 	}else {
 		div = a / b;
-		printf("%.2f", div);
+		printf("Result of division: %.2f", div);
 	}
 	system("pause");
 	return 0;
